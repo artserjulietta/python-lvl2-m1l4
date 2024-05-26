6bd4e7540c4909a687a130a058a9bbdce6a1018a
artserjulietta <110678368+artserjulietta@users.noreply.github.com>
2024-05-26 10:56:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, a = 5, b = 3):
+    await ctx.send(a+b)
+
+bot.run("")
